0513f5da6f41867b8fd6bdbcc5f9adb54d36b609
skollybaba <oluwasegunokolawole@gmail.com>
2026-09-08 16:40:53 +0100

    cap build workers to stop render OOM

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: {
+    cpus: 2,
+    memoryBasedWorkersCount: true,
   },
 };
 
